bbbc32c08596ee8a163fa21d7974eadb97259532
Kamil Józefowski <k.m.jozefowski@gmail.com>
2021-05-06 20:21:44 +0200

    Add Spring Boot Actuator - Refresh

--- src/main/java/com/kodilla/accounts/controller/AccountController.java
+++ src/main/java/com/kodilla/accounts/controller/AccountController.java
@@ -4,19 +4,33 @@ import com.kodilla.accounts.domian.AccountDto;
 import com.kodilla.accounts.mapper.AccountMapper;
 import com.kodilla.accounts.service.DbService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
+@RefreshScope
+@Slf4j
 @CrossOrigin(origins = "*")
 @RestController
 @RequestMapping("/v1")
 @RequiredArgsConstructor
 public class AccountController {
 
+    @Value("${application.allow-get-accounts}")
+    private boolean allowGetAccounts;
+
     private final DbService dbService;
     private final AccountMapper accountMapper;
 
     @RequestMapping(method = RequestMethod.GET, value = "/account/{accountId}")
-    public AccountDto getTask(@PathVariable Long accountId) throws TaskNotFoundException {
+    public AccountDto getAccount(@PathVariable Long accountId) throws TaskNotFoundException {
+        if(!allowGetAccounts){
+            log.info("Getting accounts is disabled.");
+            throw new ResponseStatusException(HttpStatus.SERVICE_UNAVAILABLE, "Getting accounts is disabled.");
+        }
         return accountMapper.mapToAccountDto(dbService.getAccount(accountId).orElseThrow(TaskNotFoundException::new));
     }
 }
